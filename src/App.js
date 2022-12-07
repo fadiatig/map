6029b363e530505c63047669127159563c67c349
atigfadi <fadiatig3@gmail.com>
2022-12-07 18:47:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./views/LandingPage";
+
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+    </Switch>
+  );
+}
+
+export default App;
